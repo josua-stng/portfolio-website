fa30282c9b86d4bcd49d7ecff8162875299aa5f4
Josua <josuasitanggang51@gmail.com>
2023-01-06 09:38:03 +0700

    adding component banner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Navbar/>
+      <Banner/>
     </div>
   );
 }
