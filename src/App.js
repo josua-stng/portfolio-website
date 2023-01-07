1a2c584bbdbf58eb3c871d662aa5f594de932344
Josua <josuasitanggang51@gmail.com>
2023-01-07 10:45:42 +0700

    fix component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
+      <div className="get-bg pb-48">
       <Navbar/>
       <Banner/>
+      </div>
     </div>
   );
 }
