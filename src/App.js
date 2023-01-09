26a6b5830afeaad3cc06bb7b59ea506a905793fe
Josua <josuasitanggang51@gmail.com>
2023-01-09 09:41:29 +0700

    adding component skills

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
-      <div className="get-bg pb-48">
+      <div className="get-bg pb-48 mb-10">
       <Navbar/>
       <Banner/>
       </div>
+      <Skills/>
     </div>
   );
 }
