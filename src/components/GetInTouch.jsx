081b1112579015b3745695f6a50bc91ceefe2f2b
Josua <josuasitanggang51@gmail.com>
2023-01-10 19:20:47 +0700

    make component touch

---
+++ src/components/GetInTouch.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GetInTouch =() =>{
+    return(
+        <div className="get_touch text-center mb-[100px]">
+           <h2 className="mb-5 font-bold">Get In Touch</h2>
+           <p className="w-[320px] mx-auto mb-5">Want to know me more? contact me on social media and i will always be open to be around you</p>
+           <button className="bg-white text-black p-3  w-[150px] rounded-lg">Come</button>
+        </div>
+    )
+}
+
+
+export default GetInTouch
