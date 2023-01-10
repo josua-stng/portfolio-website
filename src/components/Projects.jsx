7b999070a56f43f865bdb9490c83b7b752bb51dd
Josua <josuasitanggang51@gmail.com>
2023-01-10 19:21:16 +0700

    adding component project

---
+++ src/components/Projects.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import UnsplashImage from '../assets/image/unsplash-clone.jpeg'
+import NextMarketImage from '../assets/image/next_market.jpeg'
+import WeatherApp from '../assets/image/weather_app.jpeg'
+
+const Projects = () =>{
+    return (
+        <div className="project_header mb-[220px]">
+        <div className="p-7 text-center">
+        <h2 className="text-[45px] font-bold mb-5">Projects</h2>
+        <p className="text-lg">I show you to all the big and small websites I have done so farI show you to all the big and small websites I have done so far</p>
+        <div className="project grid breakpoints_skills:grid-cols-2 laptop:grid-cols-3">
+            <div className="project_container ">
+                <h3 className="text-2xl mb-5">Unsplash Clone</h3>
+                <img src={UnsplashImage} alt="" className="image_project rounded-lg"/>
+            </div>
+            <div className="project_container">
+            <h3 className="text-2xl mb-5">Next Market</h3>
+                <img src={NextMarketImage} alt="" className="image_project rounded-lg"/>
+            </div>
+            <div className="project_container">
+            <h3 className="text-2xl mb-5">Weather App</h3>
+                <img src={WeatherApp} alt="" className="image_project rounded-lg"/>
+            </div>
+        </div>
+        <p className="pointer text-sm">View more of my projects {">"} </p>
+        </div>
+        </div>
+    )
+}
+
+export default Projects
