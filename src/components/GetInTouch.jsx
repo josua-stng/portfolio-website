323b81035157d20efb70ef498553154c4e423193
Josua <josuasitanggang51@gmail.com>
2023-01-14 09:12:14 +0700

    fix styling getInTouch

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const GetInTouch = () => {
   return (
-    <div className="get_touch text-center mb-[100px]  w-[420px] p-5 mx-auto">
-      <h2 className="mb-5 font-bold">Get In Touch</h2>
+    <div className="get_touch text-center mb-[100px]  w-[420px] p-5  m-auto">
+      <h2 className="mb-5 font-bold text-2xl">Get In Touch</h2>
       <p className="w-[320px] mx-auto mb-5">
         Want to know me more? contact me on social media and i will always be
         open to be around you
