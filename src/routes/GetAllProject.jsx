bcadc2cbf7ef142da483f784e0d18a4ab17f605d
Josua <josuasitanggang51@gmail.com>
2023-01-31 09:00:31 +0700

    add href for project

--- src/routes/GetAllProject.jsx
+++ src/routes/GetAllProject.jsx
@@ -19,42 +19,70 @@ const GetAllProject = () => {
             show you to all the big and small websites I have done so far
           </p>
           <div className="project grid breakpoints_skills:grid-cols-2 laptop:grid-cols-3">
-            <div className="project_container ">
-              <h3 className="text-2xl mb-5">Unsplash Clone</h3>
-              <img
-                src={UnsplashImage}
-                alt=""
-                className="image_project rounded-lg"
-              />
-            </div>
-            <div className="project_container">
-              <h3 className="text-2xl mb-5">Next Market</h3>
-              <img
-                src={NextMarketImage}
-                alt=""
-                className="image_project rounded-lg"
-              />
-            </div>
-            <div className="project_container">
-              <h3 className="text-2xl mb-5">Weather App</h3>
-              <img
-                src={WeatherApp}
-                alt=""
-                className="image_project rounded-lg"
-              />
-            </div>
-            <div className="project_container">
-              <h3 className="text-2xl mb-5">Idmb Movie</h3>
-              <img
-                src={IdmbMovie}
-                alt=""
-                className="image_project rounded-lg"
-              />
-            </div>
+            <a
+              className="project_container"
+              href="https://unsplash-clone-apps.netlify.app/ "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <h3 className="text-2xl mb-5">Unsplash Clone</h3>
+                <img
+                  src={UnsplashImage}
+                  alt=""
+                  className="image_project rounded-lg"
+                />
+              </div>
+            </a>
+            <a
+              className="project_container"
+              href="https://nextmarket.netlify.app/index.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="project_container">
+                <h3 className="text-2xl mb-5">Next Market</h3>
+                <img
+                  src={NextMarketImage}
+                  alt=""
+                  className="image_project rounded-lg"
+                />
+              </div>
+            </a>
+            <a
+              className="project_container"
+              href="https://indonesia-weather-app.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <h3 className="text-2xl mb-5">Weather App</h3>
+                <img
+                  src={WeatherApp}
+                  alt=""
+                  className="image_project rounded-lg"
+                />
+              </div>
+            </a>
+            <a
+              href="https://sharing-session-react-001.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="project_container"
+            >
+              <div>
+                <h3 className="text-2xl mb-5">Idmb Movie</h3>
+                <img
+                  src={IdmbMovie}
+                  alt=""
+                  className="image_project rounded-lg"
+                />
+              </div>
+            </a>
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
